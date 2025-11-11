a6746c46c9de8084bf0d17a11ac2a5e34802c4ce
Ruthe <vitoriaruthe76@gmail.com>
2025-11-11 13:31:08 +0000

    nivel 1

--- xadrez.c
+++ xadrez.c
@@ -6,16 +6,57 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+
+    int bispo = 0; //para que no loop a peça se movimente 3 vezes, se for const não posso alterar o valor nem usar ++
+    int torre; //loop for, declaração do valor é feita no loop
+    int rainha = 0; //valor a ser incrementado no loop
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    while(bispo < 3 && bispo >= 0){
+        printf(".BISPO.\n");
+        printf("==CIMA==\n");
+        printf("==ESQUERDA==\n");
+        bispo++;
+    }
+
+    //avisa fim do movimento
+    if(bispo == 3){
+        printf(".Fim do movimento do Bispo.\n\n");
+    }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    for(torre = 0; torre < 2; torre++){
+        printf(".TORRE.\n");
+        printf("==DIREITA==\n");
+    }
+
+    //avisa fim do movimento
+    if(torre == 2){
+        printf(".Fim do movimento da Torre.\n\n");
+    }
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    do{
+        printf(".RAINHA.\n");
+        printf("==CIMA==\n");
+        printf("==CIMA==\n");
+        printf("==CIMA==\n");
+        rainha++;
+    } while(rainha < 5);
+
+    //avisa fim do movimento
+    if(rainha == 5){
+        printf(".Fim do movimento da Rainha.\n\n");
+    }
+
+    //avisa fim do movimento
+    if(bispo == 3 && torre == 2 && rainha == 5){
+        printf(".Fim do Jogo.\n");
+    }
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
