0f5b671e4bc45fffae143017651e5e3a9be91a9d
Ruthe <vitoriaruthe76@gmail.com>
2025-11-12 15:42:44 +0000

    nivel 2

--- xadrez.c
+++ xadrez.c
@@ -10,6 +10,7 @@ int main() {
     int bispo = 0; //para que no loop a peça se movimente 3 vezes, se for const não posso alterar o valor nem usar ++
     int torre; //loop for, declaração do valor é feita no loop
     int rainha = 0; //valor a ser incrementado no loop
+    int cavalo = 0; // se não declara o valor nem aqui nem no loop, o valor é atribido com "valor lixo"
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -52,15 +53,30 @@ int main() {
         printf(".Fim do movimento da Rainha.\n\n");
     }
 
-    //avisa fim do movimento
-    if(bispo == 3 && torre == 2 && rainha == 5){
-        printf(".Fim do Jogo.\n");
-    }
-
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    while(cavalo < 2) {
+        for(int i = 0; i < 1; i++){ //usar variavel diferente no for (i), se não gera um loop infinito, já que o for sempre reinicializa cavlo = 0
+            printf(".CAVALO.\n");
+            printf("==CIMA==\n");
+            printf("==CIMA==\n"); 
+        }
+
+        printf("==ESQUERDA==\n");
+        cavalo++; //já que usei uma variavel diferente no for, é preciso incrementar cavalo++
+
+        if (cavalo == 2){
+            printf(".Fim do movimento do cavalo.\n");
+        }
+    }
+
+    //avisa fim do jogo
+    if(bispo == 3 && torre == 2 && rainha == 5 && cavalo == 2){
+        printf(".Fim do Jogo.\n");
+    }
+
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
